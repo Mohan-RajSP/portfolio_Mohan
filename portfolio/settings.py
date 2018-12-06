562bd22a49223a308993417c381c0f09eea1dc23
Mohan Raj Paramasivam <einsatz.spmr@gmail.com>
2018-12-06 11:07:58 +0530

    local setting removed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio_DB',
-        'USER': 'postgres',
-        'PASSWORD': 'Dhanamsiva@0206',
+        'USER': 'mohan_portfolio_db',
+        'PASSWORD': 'Mon07m@n',
         'HOST': 'LOCALHOST',
         'PORT': 5433
 
@@ -131,7 +131,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   # creates a folder called "media"
 MEDIA_URL = '/media/'  # asub folder will be created under "/media/" like image, audio, video where the files wil be saved
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
